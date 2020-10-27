f5291f57155cfda597d62cf1de7c7230bfbe06ed
Telmo1996 <tfercor@gmail.com>
2020-10-27 19:24:26 +0100

    list -hid -long

--- svn/P1/shell.c
+++ svn/P1/shell.c
@@ -273,20 +273,47 @@ int cmdCreate(int argc, char *argv[]){
 }
 
 int cmdList(int argc, char *argv[]){
-	//esto lo escribe anna
     char opL=0, opD=0, opH=0, opR=0;
+	char muestra=0;
+    DIR *dirp;
+    struct dirent *direntp;
+	char * name=".";
+	int i;
     for(i=1; i<argc; i++){
-        if (strcmp(argv[i], "-long")) opL=1;
-        else if (strcmp(argv[i], "-dir")) opD=1;
-        else if (strcmp(argv[i], "-hid")) opH=1;
-        else if (string(argv[i], "-rec")) opR=1;
+        if (strcmp(argv[i], "-long") == 0) opL=1;
+        else if (strcmp(argv[i], "-dir") == 0) opD=1;
+        else if (strcmp(argv[i], "-hid") == 0) opH=1;
+        else if (strcmp(argv[i], "-rec") == 0) opR=1;
     }
 
-	//esto lo escribe telmo
-	printf("hola soy telmo");
 
+	dirp = opendir(name);
+	if(dirp == NULL){
+		printf("error\n");
+		exit(2);
+	}
+	
+	if(opL)
+		printf("i-node\toffset\t\t\tlong\tnombre\n");
+	else
+		printf("nombre\n");
+	while((direntp = readdir(dirp)) != NULL){
+		//TODO ojo con el directorio actual
+		muestra = 1;
+		if((direntp->d_name[0] == '.') && !opH)
+			muestra=0;
+		if(muestra){
+			if(opL){
+				printf("%ld\t%ld\t%d\t%s\n", direntp->d_ino, direntp->d_off,
+					direntp->d_reclen, direntp->d_name
+				);
+			}else{
+				printf("%s\n", direntp->d_name);
+			}
+		}
+	}
 
-	exit 0;
+	return 0;
 }
 
 int cmdHistoric(int argc, char *argv[]){
