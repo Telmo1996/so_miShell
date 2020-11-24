cec6588c29e83f5ca8463fc8e725ee51840617c8
annatp4 <anna.taboada00@gmail.com>
2020-11-24 18:25:33 +0100

    fecha y hora memlist

--- svn/P2/memlist.c
+++ svn/P2/memlist.c
@@ -13,23 +13,29 @@ memNode_t * memCreateList(){
     return head;
 }
 
+void timeMemory(char out[128]){
+    time_t tiempo=time(0);
+	struct tm *tlocal=localtime(&tiempo);
+	strftime(out,128,"%H:%M:%S %d/%m/%y", tlocal);
+}
+
 void memInsertElement(void *puntero, int tam, char tipo ,memNode_t * head){
     memNode_t * current = head;
-    time_t tiempo=time(0);
-    struct tm *tlocal=localtime(&tiempo);
-    char charTime[128];
-    strftime(charTime,128,"%d/%m/%y", tlocal);
-
+    char out[128];
+    
+    timeMemory(out);
     while (current->next != NULL){
         current = current->next;
     }
     current->next=(memNode_t *) malloc(sizeof(memNode_t));
     current->next->next=NULL;
     current->next->puntero = puntero;
-    current->next->fecha = charTime;
+    current->next->fecha = strdup(out);
     current->next->tam = tam;
     current->next->tipo = tipo;
 }
+
+
 
 void memPrintList(memNode_t * head, char modo) {
     memNode_t * current = head->next;
